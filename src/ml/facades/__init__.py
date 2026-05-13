79175c57a75fcf20dc420fa02c7d77d40b3264ba
LongDude <teplov.vladimir.2004@mail.ru>
2026-05-13 04:13:31 +0300

    инициализация фасадов

---
+++ src/ml/facades/__init__.py
@@ -0,0 +1,19 @@
+from ml.facades.cluster_analytics import ClusterAnalyticsFacade
+from ml.facades.cluster_dynamics import ClusterDynamicsFacade
+from ml.facades.paper_indexing import PaperIndexingFacade
+from ml.facades.recommendations import RecommendationFacade
+from ml.facades.research_entity_indexing import ResearchEntityIndexingFacade
+from ml.facades.semantic_search import SemanticSearchFacade
+from ml.facades.summaries import SummaryFacade
+from ml.facades.user_profile import UserProfileFacade
+
+__all__ = [
+    "ClusterAnalyticsFacade",
+    "ClusterDynamicsFacade",
+    "PaperIndexingFacade",
+    "RecommendationFacade",
+    "ResearchEntityIndexingFacade",
+    "SemanticSearchFacade",
+    "SummaryFacade",
+    "UserProfileFacade",
+]
