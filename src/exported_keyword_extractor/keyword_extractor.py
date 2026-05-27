df59f0e0ca388c96e1a3e30efc336069a52ea654
LongDude <teplov.vladimir.2004@mail.ru>
2026-05-27 03:39:38 +0300

    Удаление лишнего кода

--- src/exported_keyword_extractor/keyword_extractor.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from ml.facades.keyword_extraction import KeywordExtractionFacade, KeywordExtractorAdapter
-
-
-__all__ = ["KeywordExtractionFacade", "KeywordExtractorAdapter"]
